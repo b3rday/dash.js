98dc5cabb65a039a4090dfb10ccb63a03ac87419
Jeremie COLLET <jeremie.collet@orange.com>
2017-03-30 16:54:59 +0200

    HasPlayer ABR Rules :
    - Add a new sample with custom rules from Orange HasPlayer (the rules are not yet implemented)

---
+++ samples/dash-if-reference-player-orange/app/orange/config/OrangeConfig.js
@@ -0,0 +1,157 @@
+/**
+ * The copyright in this software is being made available under the BSD License,
+ * included below. This software may be subject to other third party and contributor
+ * rights, including patent rights, and no such rights are granted under this license.
+ *
+ * Copyright (c) 2013, Dash Industry Forum.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation and/or
+ *  other materials provided with the distribution.
+ *  * Neither the name of Dash Industry Forum nor the names of its
+ *  contributors may be used to endorse or promote products derived from this software
+ *  without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*global dashjs*/
+var OrangeConfig;
+
+function OrangeConfigClass (){
+    "use strict";
+
+    var paramsType = ["video", "audio"],
+
+        // Default configuration, provides list of possible parameters
+        params = {
+            "ABR.minBandwidth": -1,
+            "ABR.maxBandwidth": -1,
+            "ABR.minQuality": -1,
+            "ABR.maxQuality": -1,
+            "ABR.switchUpIncrementally": -1,
+            "ABR.switchUpRatioSafetyFactor": -1,
+            "ABR.latencyInBandwidth": -1,
+            "ABR.switchDownBufferTime": -1,
+            "ABR.switchDownBufferRatio": -1,
+            "ABR.switchLowerBufferTime": -1,
+            "ABR.switchLowerBufferRatio": -1,
+            "ABR.switchUpBufferTime": -1,
+            "ABR.switchUpBufferRatio": -1,
+            "ABR.keepBandwidthCondition": -1,
+            "ABR.droppedFramesMinRatio": -1,
+            "ABR.droppedFramesMaxRatio": -1,
+            // Video parameters
+            "video": {
+            },
+            // Audio parameters
+            "audio": {
+            }
+        },
+
+        doSetParams = function (newParams) {
+            var item,
+                typeParams,
+                typeItem;
+
+            for (item in newParams) {
+                if (newParams.hasOwnProperty(item)) {
+                    // Check if comment
+                    if (item.indexOf('//') === -1) {
+                        // Check if type parameters
+                        if (paramsType.indexOf(item) > -1) {
+                            typeParams = newParams[item];
+                            for (typeItem in typeParams) {
+                                if (typeParams.hasOwnProperty(typeItem)) {
+                                    params[item][typeItem] = newParams[item][typeItem];
+                                }
+                            }
+                        } else {
+                            params[item] = newParams[item];
+                        }
+                    }
+                }
+            }
+        },
+
+        getParam = function (params, name, type, def) {
+            var value = params[name];
+
+            if ((value === undefined) || (value === -1)) {
+                return def;
+            }
+
+            if ((type !== undefined) && (typeof value !== type)) {
+                switch (type) {
+                    case 'number':
+                        value = Number(value);
+                        break;
+                    case 'boolean':
+                        value = (value === 'true') ||
+                            (value === '1') ||
+                            (value === 1);
+                        break;
+                    default:
+                        break;
+                            }
+            }
+
+            return value;
+        },
+
+        doGetParam = function (name, type, def) {
+            return getParam(params, name, type, def);
+        },
+
+        doGetParamFor = function (key, name, type, def) {
+            var typeParams = params[key];
+
+            if ((typeParams !== undefined) && (typeParams[name] !== undefined)) {
+                return getParam(typeParams, name, type, def);
+            }
+
+            return getParam(params, name, type, def);
+        };
+
+    return {
+        debug: undefined,
+
+        setup: function () {
+        },
+
+        setParams: function (newParams) {
+            doSetParams(newParams);
+
+            // Update debugger level if set in config
+            var level = this.getParam("Debug.level", "number", -1);
+            if (level !== -1) {
+                this.debug.setLevel(level);
+            }
+        },
+
+        getParam: function (name, type, def) {
+            return doGetParam(name, type, def);
+        },
+
+        getParamFor: function (key, name, type, def) {
+            return doGetParamFor(key, name, type, def);
+        }
+    };
+}
+
+OrangeConfigClass.__dashjs_factory_name = 'OrangeConfig';
+OrangeConfig = dashjs.FactoryMaker.getSingletonFactory(OrangeConfigClass);
---
+++ samples/dash-if-reference-player-orange/app/orange/rules/OrangeAbandonRequestsRule.js
@@ -0,0 +1,115 @@
+/**
+ * The copyright in this software is being made available under the BSD License,
+ * included below. This software may be subject to other third party and contributor
+ * rights, including patent rights, and no such rights are granted under this license.
+ *
+ * Copyright (c) 2013, Dash Industry Forum.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation and/or
+ *  other materials provided with the distribution.
+ *  * Neither the name of Dash Industry Forum nor the names of its
+ *  contributors may be used to endorse or promote products derived from this software
+ *  without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*global dashjs*/
+
+var OrangeAbandonRequestsRule;
+
+function OrangeAbandonRequestsRuleClass() {
+
+    const ABANDON_MULTIPLIER = 2;
+    const GRACE_TIME_THRESHOLD = 0.5;
+
+    const context = this.context;
+    let factory = dashjs.FactoryMaker;
+
+    let SwitchRequest = factory.getClassFactoryByName('SwitchRequest');
+    let MediaPlayerModel = factory.getSingletonFactoryByName('MediaPlayerModel');
+    let DashMetrics = factory.getSingletonFactoryByName('DashMetrics');
+    let MetricsModel = factory.getSingletonFactoryByName('MetricsModel');
+    let Debug = factory.getSingletonFactoryByName('Debug');
+
+    const log = Debug(context).getInstance().log;
+
+    let fragmentDict,
+        abandonDict,
+        throughputArray,
+        mediaPlayerModel,
+        dashMetrics,
+        metricsModel;
+
+    function setup() {
+        fragmentDict = {};
+        abandonDict = {};
+        throughputArray = [];
+        mediaPlayerModel = MediaPlayerModel(context).getInstance();
+        dashMetrics = DashMetrics(context).getInstance();
+        metricsModel = MetricsModel(context).getInstance();
+    }
+
+    function shouldAbandon(rulesContext) {
+
+        const mediaInfo = rulesContext.getMediaInfo();
+        const type = mediaInfo.type;
+        var request = rulesContext.getCurrentRequest();
+        var switchRequest = SwitchRequest(context).create(SwitchRequest.NO_CHANGE, {name: OrangeAbandonRequestsRuleClass.__dashjs_factory_name});
+
+        var now = new Date().getTime(),
+            elapsedTime,
+            measuredBandwidth,
+            estimatedTimeOfDownload;
+
+        if (request.firstByteDate === null || request.aborted) {
+            log("[AbandonRequestsRule][" + type + "] Request has already been aborted.");
+            return switchRequest;
+        }
+
+        elapsedTime = (now - request.firstByteDate.getTime()) / 1000;
+
+        if (request.bytesLoaded < request.bytesTotal && elapsedTime >= (request.duration * GRACE_TIME_THRESHOLD)) {
+
+            measuredBandwidth = request.bytesLoaded / elapsedTime;
+            estimatedTimeOfDownload = request.bytesTotal / measuredBandwidth;
+
+            if ((estimatedTimeOfDownload) > (request.duration * ABANDON_MULTIPLIER)) {
+                switchRequest = SwitchRequest(context).create(0, {name: OrangeAbandonRequestsRuleClass.__dashjs_factory_name});
+                log("[AbandonRequestsRule][" + type + "] BW = " + (measuredBandwidth * 8 / 1000).toFixed(3) + " kb/s => switch to lowest quality");
+            }
+        }
+        return switchRequest;
+    }
+
+    function reset() {
+        setup();
+    }
+
+    const instance = {
+        shouldAbandon: shouldAbandon,
+        reset: reset
+    };
+
+    setup();
+    return instance;
+}
+
+OrangeAbandonRequestsRuleClass.__dashjs_factory_name = 'OrangeAbandonRequestsRule';
+OrangeAbandonRequestsRule = dashjs.FactoryMaker.getClassFactory(OrangeAbandonRequestsRuleClass);
+
