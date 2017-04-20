56f5c00c1fef58a1f62fffdaac36050f041ed033
Nicolas ANGOT <nicolas.angot@orange.com>
2017-04-20 15:23:47 +0200

    use ES6 syntax for core folder

--- src/core/Debug.js
+++ src/core/Debug.js
@@ -88,8 +88,8 @@ function Debug() {
      */
     function log() {
 
-        var message = '';
-        var logTime = null;
+        let message = '';
+        let logTime = null;
 
         if (showLogTimestamp) {
             logTime = new Date().getTime();
